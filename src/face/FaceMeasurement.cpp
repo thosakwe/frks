fa2ff6bdfd10ab65dcce512dc200e3f8d00189b8
Tobe O <thosakwe@gmail.com>
2018-03-28 10:00:24 -0400

    Style changes

--- src/face/FaceMeasurement.cpp
+++ src/face/FaceMeasurement.cpp
@@ -3,50 +3,50 @@
 //
 #include "FaceMeasurement.h"
 
-FaceMeasurement::FaceMeasurement(double center_x, double left_pupil, double right_pupil, double upper_lip,
-                                 double lower_lip, double mouth_left, double mouth_right) {
-    this->center_x = center_x;
-    this->left_pupil_location = left_pupil;
-    this->right_pupil_location = right_pupil;
-    this->upper_lip_location = upper_lip;
-    this->lower_lip_location = lower_lip;
-    this->mouth_left = mouth_left;
-    this->mouth_right = mouth_right;
+FaceMeasurement::FaceMeasurement(double pCenterX, double pLeftPupil, double pRightPupil, double pUpperLip,
+                                 double pLowerLip, double pMouthLeft, double pMouthRight) {
+    this->mCenterX = pCenterX;
+    this->mLeftPupil = pLeftPupil;
+    this->mRightPupil = pRightPupil;
+    this->mUpperLip = pUpperLip;
+    this->mLowerLip = pLowerLip;
+    this->mMouthLeft = pMouthLeft;
+    this->mMouthRight = pMouthRight;
 }
 
 double FaceMeasurement::get_center_x() const {
-    return center_x;
+    return mCenterX;
 }
 
 double FaceMeasurement::get_left_pupil() const {
-    return left_pupil_location;
+    return mLeftPupil;
 }
 
 double FaceMeasurement::get_right_pupil() const {
-    return right_pupil_location;
+    return mRightPupil;
 }
 
 double FaceMeasurement::get_upper_lip() const {
-    return upper_lip_location;
+    return mUpperLip;
 }
 
 double FaceMeasurement::get_lower_lip() const {
-    return lower_lip_location;
+    return mLowerLip;
 }
 
 double FaceMeasurement::get_mouth_left() const {
-    return mouth_left;
+    return mMouthLeft;
 }
 
 double FaceMeasurement::get_mouth_right() const {
-    return mouth_right;
+    return mMouthRight;
 }
 
 double FaceMeasurement::mouth_width() const {
-    return  mouth_right - mouth_left;
+    return  mMouthRight - mMouthLeft;
 }
 
 
 double FaceMeasurement::mouth_height() const {
-    return lower_lip_location - upper_lip_location;
+    return mLowerLip - mUpperLip;
 }
--- src/face/FaceMeasurement.h
+++ src/face/FaceMeasurement.h
@@ -9,28 +9,34 @@
  * @brief An abstraction over measurements of different facial features.
  */
 class FaceMeasurement {
-private:
-    double center_x;
-    double left_pupil_location, right_pupil_location;
-    double upper_lip_location, lower_lip_location;
-    double mouth_left, mouth_right;
 
 public:
-    FaceMeasurement(double center_x, double left_pupil, double right_pupil, double upper_lip, double lower_lip, double mouth_left,
-                    double mouth_right);
+    FaceMeasurement(double pCenterX, double pLeftPupil, double pRightPupil, double pUpperLip, double pLowerLip,
+                    double pMouthLeft, double pMouthRight);
 
     // Getters
     double get_center_x() const;
+
     double get_left_pupil() const;
+
     double get_right_pupil() const;
+
     double get_upper_lip() const;
+
     double get_lower_lip() const;
+
     double get_mouth_left() const;
+
     double get_mouth_right() const;
 
     // Computations
     double mouth_width() const;
+
     double mouth_height() const;
+
+
+private:
+    double mCenterX, mLeftPupil, mRightPupil, mUpperLip, mLowerLip, mMouthLeft, mMouthRight;
 };
 
 #endif //FRKS_FACEMEASUREMENT_H
