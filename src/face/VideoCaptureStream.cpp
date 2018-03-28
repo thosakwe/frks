3fda2936ca7f0cbb62ed766cad22790d867751de
Tobe O <thosakwe@gmail.com>
2018-03-28 09:49:02 -0400

    Link OpenCV

---
+++ src/face/VideoCaptureStream.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tobe on 3/28/18.
+//
+
+#include "VideoCaptureStream.h"
---
+++ src/face/VideoCaptureStream.h
@@ -0,0 +1,14 @@
+//
+// Created by Tobe on 3/28/18.
+//
+
+#ifndef FRKS_VIDEOCAPTURESTREAM_H
+#define FRKS_VIDEOCAPTURESTREAM_H
+
+
+class VideoCaptureStream {
+
+};
+
+
+#endif //FRKS_VIDEOCAPTURESTREAM_H
