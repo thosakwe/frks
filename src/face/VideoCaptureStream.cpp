18614900be10158f9c208cb57b46a4b338f4b2bf
Tobe O <thosakwe@gmail.com>
2018-03-28 10:50:02 -0400

    Detect eyes

--- src/face/VideoCaptureStream.cpp
+++ src/face/VideoCaptureStream.cpp
@@ -33,13 +33,38 @@ VideoCaptureStream::convert_mat(cv::Mat mat, cv::CascadeClassifier *pFaceCascade
     cv::Mat gray;
     cv::cvtColor(mat, gray, cv::COLOR_BGR2GRAY);
 
-    // Detect faces
-    std::vector<cv::Rect> faces;
-    pFaceCascade->detectMultiScale(gray, faces, 1.3, 5);
+    // Detect eyes first.
+    std::vector<cv::Rect> eyes;
 
-    cv::namedWindow("w", cv::WINDOW_AUTOSIZE);
-    cv::imshow("w", gray);
-    cv::waitKey(0);
-    cv::destroyAllWindows();
+    pEyeCascade->detectMultiScale(gray, eyes, 1.3, 5);
+
+    if (eyes.size() < 2)
+        return nullptr;
+
+    cv::Rect leftEye, rightEye;
+
+    // Find the leftmost eye
+    int x = eyes.at(0).x;
+
+    for (auto eye : eyes) {
+        if (eye.x > x) {
+            x = (leftEye = eye).x;
+        }
+    }
+
+    // Then, find the rightmost
+    for (auto eye : eyes) {
+        if (eye.x < x) {
+            x = (rightEye = eye).x;
+        }
+    }
+
+    cv::rectangle(mat, rightEye, cv::Scalar(255, 0, 0), 10);
+    cv::rectangle(mat, leftEye, cv::Scalar(0, 255, 0), 10);
+
+    //cv::imshow("w", mat);
+    //cv::waitKey(1);
+
+    //cv::destroyAllWindows();
     return nullptr;
 }
